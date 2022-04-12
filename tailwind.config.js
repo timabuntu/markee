14952425121fe2cd5e0fa31df4a0801daf485eb8
thiago lopes de mello <timabuntu@gmail.com>
2022-04-12 15:47:35 -0300

    config: fonts DM sans and Inconsolata

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dm_sans: ['DM sans'],
+        inconsolata: ['Inconsolata'],
+      },
+    },
   },
   plugins: [],
 };
